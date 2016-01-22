c05db03e16659b920ae0a1033d00c40b23f674f8
Ken McCullagh <ken.mccullagh@s3group.com>
2016-01-22 12:58:40 +0000

    dunno

--- CheerLightsHistory/TweetingCheerLightsPrivate.h
+++ CheerLightsHistory/TweetingCheerLightsPrivate.h
@@ -1,4 +1,4 @@
-//const char* token="4059494159-aI1CkCcDOsAPTek1yAt4c5A3hcyd5UrffHapXaJ";
 
-const char* ssid     = "S3AIR";
-const char* password = "LovinglyDelig";
+
+const char* ssid[]     = {"scribble", "glassroom2", "S3AIR"};
+const char* password[] = {"BYULNZXP","S3demos-wifi", "PoorBreathedU"};
